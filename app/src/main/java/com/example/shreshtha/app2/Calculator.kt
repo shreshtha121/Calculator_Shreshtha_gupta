3d8e657ab53384098ad1eefb0611fb967d2ca883
Shreshtha Gupta <shreshthagupta20@gmail.com>
2017-09-14 18:43:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shreshtha/app2/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.shreshtha.app2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
